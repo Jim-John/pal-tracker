441a743308ec920516b8c5b2a93846baf46247ea
John <696449@cognizant.com>
2020-03-03 17:16:37 +0530

    Environmnet commit

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -2,12 +2,20 @@ package io.pivotal.pal.tracker;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 public class WelcomeController {
 
+    private String welcomeMsg;
+
+    public WelcomeController(
+            @Value("${welcome.message}") String welcomeMsg){
+        this.welcomeMsg=welcomeMsg;
+    }
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return welcomeMsg;
     }
 }
