15a442a2ca628f913756825f84c15d06e0aff60c
John <696449@cognizant.com>
2020-03-04 18:43:33 +0530

    spring mvc final

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,7 +1,7 @@
 package io.pivotal.pal.tracker;
 
 
-import javax.sql.DataSource;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
