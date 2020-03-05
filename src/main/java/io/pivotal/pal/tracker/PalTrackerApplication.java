778c3e4cf40ea55d37a5665ef8efa81ceb83e980
John <696449@cognizant.com>
2020-03-05 14:22:40 +0530

    First Submit

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,7 +1,7 @@
 package io.pivotal.pal.tracker;
 
 
-
+import javax.sql.DataSource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,14 +15,14 @@ public class PalTrackerApplication {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
 
-    @Bean("timeEntryRepository")
+    @Bean
     TimeEntryRepository timeEntryRepository(){
         return new InMemoryTimeEntryRepository();
     }
 
-  /*  @Primary
+    @Primary
     @Bean("jdbcRepo")
     JdbcTimeEntryRepository jdbcTimeEntryRepository(DataSource ds){
         return new JdbcTimeEntryRepository(ds);
-    }*/
+    }
 }
